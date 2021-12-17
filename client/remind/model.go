66d107d7f10b62509dd7a578aa2471a64f6c8775
zongxingquan <zongxingquan@126.com>
2021-12-18 00:38:39 +0800

    remind

--- client/remind/model.go
+++ client/remind/model.go
@@ -1,1 +1,17 @@
 package remind
+
+import "asyncservice/rpc/remind/pb"
+
+func toRemindInfo(uid int64, rType int32) *remind_service.RemindInfo {
+	return &remind_service.RemindInfo{
+		Uid:        uid,
+		RemindType: rType,
+	}
+}
+
+func toBatchRemindRequest(uids []int64, rType int32) *remind_service.RemindBatchRequest {
+	return &remind_service.RemindBatchRequest{
+		Uids:       uids,
+		RemindType: rType,
+	}
+}
