91e746974ef2cdfd362f04271a7e10474c9679e4
zongxingquan <zongxingquan@126.com>
2022-01-21 23:41:59 +0800

    log

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -10,25 +10,21 @@ import (
 )
 
 func InitConsumer(broker []string, topic string) {
-	global.InfoLog.Printf("broker %v topic %v", broker, topic)
 	consumer, err := kafka.NewKafkaConsumer(broker)
 	defer consumer.Close()
 	partitions, err := consumer.Partitions(topic)
-	global.InfoLog.Printf("partitions %v err %v", partitions, err)
 	if err != nil {
 		return
 	}
 
 	for _, p := range partitions {
 		partitionConsumer, err := consumer.ConsumePartition(topic, p, sarama.OffsetNewest)
-		global.InfoLog.Printf("p %v partitionconsumer %#v err %v", p, partitionConsumer, err)
 		if err != nil {
 			global.ExcLog.Printf("partitionconsumer err %v", err)
 			continue
 		}
 
 		for m := range partitionConsumer.Messages() {
-			global.InfoLog.Printf("p %v m %#v", p, string(m.Value))
 			process(m)
 		}
 	}
@@ -36,7 +32,7 @@ func InitConsumer(broker []string, topic string) {
 
 func process(message *sarama.ConsumerMessage) {
 	val := message.Value
-	global.ExcLog.Printf("ProcessComics: info, key %v value %v", string(message.Key), string(message.Value))
+	global.InfoLog.Printf("ProcessComics: info, key %v value %v", string(message.Key), string(message.Value))
 	event := new(kafka.KafkaMessage)
 	err := json.Unmarshal(val, event)
 	if err != nil {
