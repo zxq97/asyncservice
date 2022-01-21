ac64962d8c29892f0f7fb6699cc46e776189e7c1
zxq <zongxingquan@126.com>
2022-01-22 00:07:42 +0800

    follow

--- consumer/social/social.go
+++ consumer/social/social.go
@@ -1,9 +1,17 @@
 package social
 
-import "asyncservice/client/kafka"
+import (
+	"asyncservice/client/kafka"
+	"asyncservice/client/user"
+	"asyncservice/global"
+	"context"
+)
 
 func Follow(follow *kafka.KafkaMessage) {
-
+	uid := follow.Info.UID
+	touid := follow.Info.ToUID
+	userMap, err := user.GetBatchUserinfo(context.Background(), []int64{uid, touid})
+	global.InfoLog.Printf("uid %v touid %v usermap %v err %v", uid, touid, userMap, err)
 }
 
 func Unfollow(unfollow *kafka.KafkaMessage) {
